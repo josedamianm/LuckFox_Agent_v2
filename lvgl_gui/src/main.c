636cc24473e2e6acdcc671e2ba53f3fbdb08c5a3
josemanco <josedamianm@gmail.com>
2026-03-16 15:24:04 +0100

    Use lv_refr_now to force immediate screen refresh on button press

--- lvgl_gui/src/main.c
+++ lvgl_gui/src/main.c
@@ -67,8 +67,7 @@ int main(void) {
                     case 2: set_screen_color(lv_color_hex(0xFFFF00), "YELLOW\n\nA=Red  B=Blue\nX=Yellow  Y=Green"); break;
                     case 3: set_screen_color(lv_color_hex(0x00FF00), "GREEN\n\nA=Red  B=Blue\nX=Yellow  Y=Green"); break;
                 }
-                lv_obj_invalidate(lv_screen_active());
-                lv_timer_handler();
+                lv_refr_now(NULL);
             }
             prev[i] = pressed;
         }
