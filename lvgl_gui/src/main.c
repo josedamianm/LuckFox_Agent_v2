f634a621286756902d5e551423dfb4b333aa0f82
josemanco <josedamianm@gmail.com>
2026-03-16 14:38:06 +0100

    main: minimal color-test, buttons A/B/X/Y change screen color

--- lvgl_gui/src/main.c
+++ lvgl_gui/src/main.c
@@ -1,15 +1,12 @@
 #include "lvgl.h"
 #include "hal/disp_driver.h"
 #include "hal/indev_buttons.h"
-#include "ipc/ipc_server.h"
-#include "ipc/cmd_parser.h"
-#include "screens/scr_manager.h"
-#include "theme/lf_theme.h"
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
 
 static volatile int g_running = 1;
+static lv_obj_t *g_scr = NULL;
 
 static void sig_handler(int sig) {
     (void)sig;
@@ -17,16 +14,19 @@ static void sig_handler(int sig) {
 }
 
 static void on_button(btn_id_t btn, bool pressed) {
-    static const char *names[] = {
-        "A", "B", "X", "Y", "UP", "DOWN", "LEFT", "RIGHT", "CTRL"
-    };
-    if (btn < BTN_COUNT && pressed) {
-        char msg[64];
-        snprintf(msg, sizeof(msg),
-                 "{\"event\":\"button\",\"name\":\"%s\",\"state\":\"pressed\"}",
-                 names[btn]);
-        ipc_server_broadcast(msg);
+    if (!pressed || !g_scr) return;
+    lv_color_t c;
+    const char *name;
+    switch (btn) {
+        case BTN_A: c = lv_color_make(0xFF, 0x00, 0x00); name = "A - RED";    break;
+        case BTN_B: c = lv_color_make(0x00, 0x00, 0xFF); name = "B - BLUE";   break;
+        case BTN_X: c = lv_color_make(0xFF, 0xFF, 0x00); name = "X - YELLOW"; break;
+        case BTN_Y: c = lv_color_make(0x00, 0xFF, 0x00); name = "Y - GREEN";  break;
+        default:    return;
     }
+    printf("Button %s\n", name);
+    lv_obj_set_style_bg_color(g_scr, c, 0);
+    lv_obj_invalidate(g_scr);
 }
 
 int main(void) {
@@ -35,28 +35,30 @@ int main(void) {
 
     lv_init();
 
-    lv_display_t *disp  = disp_driver_init();
-    lv_indev_t   *indev = indev_buttons_init();
+    disp_driver_init();
+    lv_indev_t *indev = indev_buttons_init();
+    (void)indev;
 
-    lf_theme_init(disp);
-    lv_indev_set_group(indev, lv_group_get_default());
+    g_scr = lv_obj_create(NULL);
+    lv_obj_set_style_bg_color(g_scr, lv_color_make(0x00, 0x00, 0x80), 0);
+    lv_obj_set_style_bg_opa(g_scr, LV_OPA_COVER, 0);
 
-    scr_manager_init();
-    scr_manager_switch(SCR_STATUS);
+    lv_obj_t *lbl = lv_label_create(g_scr);
+    lv_label_set_text(lbl, "A=Red  B=Blue\nX=Yellow Y=Green");
+    lv_obj_set_style_text_color(lbl, lv_color_white(), 0);
+    lv_obj_center(lbl);
 
-    ipc_server_init("/tmp/luckfox_gui.sock");
-    cmd_parser_init();
+    lv_scr_load(g_scr);
+
     indev_buttons_set_event_cb(on_button);
 
-    printf("luckfox_gui running\n");
+    printf("color_test running\n");
 
     while (g_running) {
         lv_timer_handler();
-        ipc_server_poll();
         usleep(5000);
     }
 
-    ipc_server_deinit();
     indev_buttons_deinit();
     disp_driver_deinit();
     return 0;
